0cb438a4633ee4c532bad693beec58cf9d7787c7
JUKKarol <karolprzybylak2@gmail.com>
2025-04-13 19:31:54 +0200

    Init

---
+++ geometry-api/src/main/java/com/github/jukkarol/geometry_api/GeometryApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jukkarol.geometry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryApiApplication.class, args);
+	}
+
+}
