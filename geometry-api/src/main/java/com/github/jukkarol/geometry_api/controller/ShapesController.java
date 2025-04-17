98492e2519ce50719a544cb4d15842735edbb9cc
JUKKarol <karolprzybylak2@gmail.com>
2025-04-17 19:41:42 +0200

    Add functions to ShapesController

--- geometry-api/src/main/java/com/github/jukkarol/geometry_api/controller/ShapesController.java
+++ geometry-api/src/main/java/com/github/jukkarol/geometry_api/controller/ShapesController.java
@@ -3,6 +3,7 @@ package com.github.jukkarol.geometry_api.controller;
 import com.github.jukkarol.geometry_api.dto.shapeDto.DisplayShapeDto;
 import com.github.jukkarol.geometry_api.dto.shapeDto.request.CreateShapeRequest;
 import com.github.jukkarol.geometry_api.dto.shapeDto.request.GetAllShapesRequest;
+import com.github.jukkarol.geometry_api.service.ShapeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +14,18 @@ import java.util.List;
 @RequestMapping("api/v1/shapes")
 @RestController
 public class ShapesController {
-    @PostMapping("/signup")
-    public ResponseEntity<CreateShapeRequest> register(@RequestBody CreateShapeRequest createShapeRequest) {
-        CreateShapeRequest createdShape =
+    public final ShapeService shapeService;
+
+    @PostMapping()
+    public ResponseEntity<DisplayShapeDto> register(@RequestBody CreateShapeRequest createShapeRequest) {
+        DisplayShapeDto createdShape = shapeService.CreateShape(createShapeRequest);
 
         return ResponseEntity.ok(createdShape);
     }
 
-    @GetMapping("/signup")
+    @GetMapping()
     public ResponseEntity<List<DisplayShapeDto>> register(@RequestBody GetAllShapesRequest getAllShapesRequest) {
-        List<DisplayShapeDto> shapes =
+        List<DisplayShapeDto> shapes = shapeService.GetShapesByType(getAllShapesRequest.getType());
 
         return ResponseEntity.ok(shapes);
     }
