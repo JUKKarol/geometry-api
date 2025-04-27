27124516dd16e2f3847e905f3b925bd97cff194e
JUKKarol <karolprzybylak2@gmail.com>
2025-04-27 16:17:58 +0200

    ReDir for microservice architecture

--- figures-service/src/main/java/com/github/jukkarol/geometry_api/FiguresServiceApplication.java
+++ figures-service/src/main/java/com/github/jukkarol/geometry_api/FiguresServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeometryApiApplication {
+public class FiguresServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeometryApiApplication.class, args);
+		SpringApplication.run(FiguresServiceApplication.class, args);
 	}
 
 }
