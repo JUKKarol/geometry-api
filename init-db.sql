c43272d8174d2698ce1bae72b097197765535246
JUKKarol <karolprzybylak2@gmail.com>
2025-05-01 09:53:53 +0200

    Add docker

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "figures-service";
+CREATE DATABASE "notification-service";
+
+GRANT ALL PRIVILEGES ON DATABASE "figures-service" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "notification-service" TO postgres;
