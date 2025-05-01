727e8da1f4891a92a95729f71af0e5e1e381471e
JUKKarol <karolprzybylak2@gmail.com>
2025-05-01 11:25:23 +0200

    Fix api-gateway for docker

--- figures-service/src/main/java/com/github/jukkarol/geometry_api/client/NotificationClient.java
+++ figures-service/src/main/java/com/github/jukkarol/geometry_api/client/NotificationClient.java
@@ -8,25 +8,24 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 @Component
 @Slf4j
 public class NotificationClient {
     private final RestTemplate restTemplate;
-    private final Environment environment;
 
-    public NotificationClient(RestTemplate restTemplate, Environment environment) {
+    @Value("${api.gateway.host}")
+    private String gatewayHost;
+
+    @Value("${api.gateway.port}")
+    private String gatewayPort;
+
+    public NotificationClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.environment = environment;
     }
 
     public void sendNotification(Long figureId) {
         try {
-            String host = InetAddress.getLocalHost().getHostAddress();
-            String port = environment.getProperty("spring.api.gateway.port");
-            String url = "http://" + host + ":" + port + "/notifications/api/v1/notifications";
+            String url = "http://" + gatewayHost + ":" + gatewayPort + "/notifications/api/v1/notifications";
 
             CreateNotificationRequest notificationRequest = new CreateNotificationRequest(figureId);
             restTemplate.postForEntity(
@@ -37,9 +36,6 @@ public class NotificationClient {
             log.info("Notification sent successfully for figure ID: {}", figureId);
         } catch (RestClientException e) {
             log.error("Failed to send notification for figure ID: {}. Error: {}", figureId, e.getMessage());
-        } catch (UnknownHostException e) {
-            log.error("Failed to determine host address. Error: {}", e.getMessage());
-            throw new RuntimeException("Failed to determine host address", e);
         }
     }
 }
